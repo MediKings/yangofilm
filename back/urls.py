9059858a2d4489ec5e20c9ce6ab40608a09213ae
Medi King's <jehovahmbote3@gmail.com>
2022-05-08 22:50:46 +0200

    Ajout des urls du backapp

--- back/urls.py
+++ back/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Admin
+from .views import Admin, Authors, AddPost, UpdatePost, DeletePost
 
 
 urlpatterns = [
     path('admin/', Admin, name='admin'),
+    path('authors/', Authors, name='authors'),
+    path('add_post/', AddPost.as_view(), name='add_post'),
+    path('update_post/<slug:slug>/', UpdatePost.as_view(), name='update_post'),
+    path('delete_post/<slug:slug>/', DeletePost.as_view(), name='delete_post'),
 ]
