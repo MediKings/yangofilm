8a42aa2085472194cf96ec774099f5eae5ed06d9
MediKings <jehovahmbote3@gmail.com>
2022-10-30 15:49:35 +0100

    Config for deployment

--- yangofilm/settings.py
+++ yangofilm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@41l%c&ez7_zsfyid%jn!4jy@582nl9-y9=@2tl@s^)aet^a$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
